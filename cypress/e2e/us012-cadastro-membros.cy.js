d2b81c3570d69ff1020a807420b08c5caef3a14d
Lívia Barbosa <liviabarbosa.aluno@unipampa.edu.br>
2025-04-27 12:21:52 -0300

    Melhoria e adição de mais testes relacionados à funcionalida de cadastro de membros.

---
+++ cypress/e2e/us012-cadastro-membros.cy.js
@@ -0,0 +1,70 @@
+/// <reference types="cypress"/>
+
+describe('US012 - Funcionalidade: Cadastro de Membros', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+    it('Cadastro de usuário válido', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', 'Barbosa', email, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
+    })
+
+    it('Cadastro de usuário com email duplicado', () => {
+      cy.preencherCadastro('Lívia', 'Barbosa', 'livia@teste.com', '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Este email já está cadastrado.')
+    })
+
+    it('Cadastro de usuário com nome vazio', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro(null, 'Barbosa', email, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Nome não pode estar vazio')
+    })
+
+    it('Cadastro de usuário com nome inválido', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia70', 'Barbosa', email, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+    })
+
+    it('Cadastro de usuário com sobrenome vazio', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', null, email, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Sobrenome não pode estar vazio')
+    })
+
+    it('Cadastro de usuário com sobrenome inválido', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', 'Barbosa@', email, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+    })
+
+    it('Cadastro de usuário com email vazio', () => {
+      cy.preencherCadastro('Lívia', 'Barbosa', null, '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'E-mail não pode estar vazio')
+    })
+
+    it('Cadastro de usuário com email inválido', () => {
+      cy.preencherCadastro('Lívia', 'Barbosa', 'livia.com', '5512345678', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'E-mail deve ser um email válido')
+    })
+
+    it('Cadastro de usuário com telefone inválido', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', 'Barbosa', email, 'teste', 'Teste!70')
+      cy.get('#signup-response').should('contain', 'Telefone deve conter apenas números')
+    })
+
+    it('Cadastro de usuário com senha vazia', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', 'Barbosa', email, null, null)
+      cy.get('#signup-response').should('contain', 'Senha não pode estar vazia')
+    })
+
+    it.only('Cadastro de usuário com senha inválida', () => {
+      var email = `livia${Date.now()}@teste.com`
+      cy.preencherCadastro('Lívia', 'Barbosa', email, null, 'teste')
+      cy.get('#signup-response').should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*)')
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('preencherCadastro', (nome, sobrenome, email, telefone, senha) => {
+    if (nome !== null) {
+        cy.get('#signup-firstname').type(nome)
+    }
+    if (sobrenome !== null) {
+        cy.get('#signup-lastname').type(sobrenome)
+    }
+    if (email !== null) {
+        cy.get('#signup-email').type(email)
+    }
+    if (telefone !== null) {
+        cy.get('#signup-phone').type(telefone)
+    }
+    if (senha !== null) {
+        cy.get('#signup-password').type(senha)
+    }
+    cy.get('#signup-button').click()
+})
